cee3b0009bd66c3298720aa0a91c0b805371cb13
Nick Roberts <roberts.nicholas88@gmail.com>
2023-06-30 13:49:45 -0400

    made window with green circle on screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-int main(int argc, char*[]) {
-    std::cout << "No errors...yet...\n";
+int main(int argc, char *[])
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Works!");
+    sf::CircleShape circle(100.f);
+    circle.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(circle);
+        window.display();
+    }
+
     return 0;
 }
