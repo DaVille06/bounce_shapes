ea02fb45f9422a4f63d72fd600d38d26a851ed26
Nick Roberts <roberts.nicholas88@gmail.com>
2023-06-30 23:32:10 -0400

    add in reading from file and handling window width and height dynamically

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,61 @@ Purpose:    Load in shapes and settings from a config file.
 
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 int main(int argc, char *[])
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Bounce Shapes");
+    std::vector<sf::CircleShape> circles;
+    std::vector<sf::RectangleShape> rectangles;
+    int windowWidth = 0, windowHeight = 0;
+    std::string fileName = "shapeconfig.txt";
+
+    std::ifstream fin(fileName);
+    // config code tells us what the line is
+    // ex. rectangle, window, font, etc.
+    std::string configCode;
+    while (fin >> configCode)
+    {
+        if (configCode == "Window")
+        {
+            // read in the next 2 tokens into width and height
+            fin >> windowWidth;
+            fin >> windowHeight;
+            std::cout << "Window width " << windowWidth << " and height " << windowHeight << " loaded...\n";
+        }
+    }
+
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Bounce Shapes");
     window.setFramerateLimit(60);
     sf::CircleShape circle(25.f);
     circle.setFillColor(sf::Color::Green);
     circle.setPosition(50.0f, 50.0f);
     sf::Vector2f velocity(1.0f, 2.0f);
-    float circleMoveSpeed = 1.0f;
 
+    int r = 0, g = 0, b = 0;
+
+    // loop after read in values
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            switch (event.type)
             {
+            // window closed
+            case sf::Event::Closed:
                 window.close();
+                break;
+            case sf::Event::KeyPressed:
+                if (event.key.code == sf::Keyboard::C)
+                {
+                    std::cout << "Change color initiated\n";
+                    circle.setFillColor(sf::Color::Magenta);
+                }
+                break;
+            default:
+                break;
             }
         }
 
